ec6c9b9471691067a5f4e551bf90e4b07a71498c
Karen Polanco <polanco.k@gmail.com>
2022-11-21 19:59:36 -0500

    commit tree c426d4b6822aac31075831e61f1477285921db99

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// .map and .split (" ") 
+// what 
+// does 
+// the 
+// this 
+// keyword 
+// mean
+// ?
+
+// use variables to make it easier to manipulate the next steps in complex functions
+//can change together methods but don't make them too long
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ")
+    let capWord = words.map(letters => letters.charAt(0).toUpperCase() + letters.slice(1)) //the reason we use slice is because we want to attach the upper cased letter to the rest of the word. get rid of the lower cased letter
+    let backTogether = capWord.join(" ")
+    return backTogether
+  })
 }
